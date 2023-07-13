933fc1c852ee9980a833a2a3b6df71eb6db645f8
justnchn <113207233+justnchn@users.noreply.github.com>
2023-07-13 17:47:46 -0400

    Add files via upload

---
+++ residualComparedWithTotal/source.R
@@ -0,0 +1,8 @@
+# source file to run all code
+
+source("~/Desktop/Research/Old Residual/functions.R")
+source("~/Desktop/Research/Old Residual/mortality.R")
+source("~/Desktop/Research/Old Residual/vaccineUptake.R")
+source("~/Desktop/Research/Old Residual/addingPopulation.R")
+source("~/Desktop/Research/Old Residual/addingCovidDeath.R")
+source("~/Desktop/Research/Old Residual/testing.R")
